33bd22b1ed64f05dda3d13892e55ba9530628ce1
Dmytro Honchar <demetrgonchar@gmail.com>
2023-10-15 21:00:27 +0300

    fix: add ignore lock files

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
 import { useState } from "react";
 
-function App() {
-  const [state, setState] = useState(0)
+const App = () => {
+  const [state, setState] = useState(0);
   const inc = () => {
-    setState(prev => prev + 1)
-  }
-  return <><button data-testid="test-title" style={{ color: 'green'}} onClick={inc}>increase</button>{state}</>
+    setState((prev) => prev + 1);
+  };
+  return (
+    <>
+      <button data-testid="test-title" style={{ color: "green" }} onClick={inc}>
+        increase
+      </button>
+      {state}
+    </>
+  );
 }
 
 export default App;

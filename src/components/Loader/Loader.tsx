f78596eb2f68dcd1034cc476e75f7c3902de603a
Dmytro Honchar <demetrgonchar@gmail.com>
2023-10-24 17:02:43 +0300

    feature: add loader component and checks logic

---
+++ src/components/Loader/Loader.tsx
@@ -0,0 +1,17 @@
+import { Box, CircularProgress } from "@mui/material";
+import { styles } from "@/components/Loader/Loader.styles";
+import { FC } from "react";
+
+interface LoaderProps {
+  size?: number;
+}
+
+const Loader: FC<LoaderProps> = ({ size = 100 }) => {
+  return (
+    <Box sx={styles.root}>
+      <CircularProgress size={size} />
+    </Box>
+  );
+};
+
+export default Loader;

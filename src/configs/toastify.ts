02077b1673adeeeac9cd49c41363c1bb47fb550f
Artur Bekh <artur@Arturs-MacBook-Pro.local>
2023-10-26 14:20:16 +0300

    fix conflicts

--- src/configs/toastify.ts
+++ src/configs/toastify.ts
@@ -1,6 +1,8 @@
 import { ToastOptions } from "react-toastify";
 
 export const baseToastifyConfig: ToastOptions = {
-    autoClose: 1800,
-    theme: 'dark'
-}
+  autoClose: 1800,
+  theme: "dark",
+  position: "top-center",
+  hideProgressBar: true,
+};

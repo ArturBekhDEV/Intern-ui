014795a523a60c6c010baa391f5f7dba740d0e7e
Dmytro Honchar <demetrgonchar@gmail.com>
2023-10-26 13:54:32 +0300

    feature: add modal

---
+++ src/components/Modal/Modal.tsx
@@ -0,0 +1,21 @@
+import { Box } from "@mui/material";
+import { FC, ReactNode } from "react";
+import { styles } from "./Modal.styles";
+import { Modal as MUIModal, ModalProps as MUIModalProps } from "@mui/material";
+
+interface ModalProps
+  extends Omit<MUIModalProps, "children" | "open" | "handleClose"> {
+  children: ReactNode;
+  isOpen: boolean;
+  handleClose: () => void;
+}
+
+const Modal: FC<ModalProps> = ({ children, isOpen, handleClose, ...props }) => {
+  return (
+    <MUIModal onClose={handleClose} open={isOpen} {...props}>
+      <Box sx={styles.root}>{children}</Box>
+    </MUIModal>
+  );
+};
+
+export default Modal;
